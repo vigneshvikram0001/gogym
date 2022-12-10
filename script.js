18bd053dfad0b32feaf5fa40ffc8effc2dc3d4e4
vigneshvikram0001 <itsmylaptop0001@gmail.com>
2022-12-10 07:29:41 +0530

    added index

--- script.js
+++
@@ -1,6 +0,0 @@
-var bars = document.querySelector("#bars");
-var content = document.querySelector(".toggle-content");
-
-bars.addEventListener("click", ()=>{
-    content.classList.toggle("active");
-})
